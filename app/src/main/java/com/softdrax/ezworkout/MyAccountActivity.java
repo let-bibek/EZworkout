c64491861fb063665c95b04858ed7b5b23723711
let_bibek <kumarbibek094@gmail.com>
2023-02-27 18:50:36 +0545

    dashboard

--- app/src/main/java/com/softdrax/ezworkout/MyAccountActivity.java
+++ app/src/main/java/com/softdrax/ezworkout/MyAccountActivity.java
@@ -8,8 +8,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -21,11 +24,16 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.util.ArrayList;
+
 public class MyAccountActivity extends AppCompatActivity {
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
     Button btnSignOut;
     TextView tvMyAccountUserName, tvUserEmail;
+    ListView lvSettings;
+    String[] settings={"Subscribe","Message us","Terms and conditions","Check for updates","About us"};
+    int[] settingsIcons={R.drawable.ic_baseline_receipt_24,R.drawable.ic_baseline_message_24,R.drawable.ic_baseline_book_24,R.drawable.ic_baseline_system_update_24,R.drawable.ic_baseline_info_24};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,16 @@ public class MyAccountActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+//        setting
+        lvSettings=findViewById(R.id.lvSettings);
+        AccountSettingAdapter accountSettingAdapter=new AccountSettingAdapter();
+
+        lvSettings.setAdapter(accountSettingAdapter);
+
+
+
+
+//        google authentication
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(MyAccountActivity.this, gso);
         btnSignOut = findViewById(R.id.btnSignOut);
@@ -55,9 +73,9 @@ public class MyAccountActivity extends AppCompatActivity {
             String userName = account.getDisplayName();
             String userEmail = account.getEmail();
             Uri imageUri = account.getPhotoUrl();
-            if (imageUri!=null){
+            if (imageUri != null) {
                 Glide.with(this).load(imageUri).into(ivUserImage);
-            }else{
+            } else {
                 Glide.with(this).load("https://th.bing.com/th/id/OIP.w-L3HP_7QYalYXw7apT2tAHaHx?pid=ImgDet&rs=1").into(ivUserImage);
             }
             tvMyAccountUserName.setText(userName);
@@ -83,4 +101,30 @@ public class MyAccountActivity extends AppCompatActivity {
     }
 
 
+    private class AccountSettingAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return settingsIcons.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view=getLayoutInflater().inflate(R.layout.settings_list,null);
+            TextView textView=view.findViewById(R.id.tvSetting);
+            ImageView imageView=view.findViewById(R.id.ivSetting);
+            textView.setText(settings[position]);
+            imageView.setImageResource(settingsIcons[position]);
+            return view;
+        }
+    }
 }
